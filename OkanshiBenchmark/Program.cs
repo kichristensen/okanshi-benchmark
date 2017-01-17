524caaee1ac21afc6507e70e0e8d60b2a6b97f44
Kim Christensen <KMCH@simcorp.com>
2017-01-17 14:59:01 +0100

    Add BasicTimer benchmark

--- OkanshiBenchmark/Program.cs
+++ OkanshiBenchmark/Program.cs
@@ -1,9 +1,10 @@
-﻿namespace OkanshiBenchmark
-{
-    public class Program
-    {
-        public static void Main()
-        {
+﻿using BenchmarkDotNet.Running;
+
+namespace OkanshiBenchmark {
+    public class Program {
+        public static void Main() {
+            var switcher = new BenchmarkSwitcher(typeof(Program).Assembly);
+            switcher.RunAll();
         }
     }
 }
---
+++ OkanshiBenchmark/StopwatchVsBasicTimer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+using BenchmarkDotNet.Attributes;
+using Okanshi;
+
+namespace OkanshiBenchmark {
+    public class StopwatchVsBasicTimer {
+        private readonly BasicTimer basicTimer = OkanshiMonitor.BasicTimer("Test");
+        private readonly Stopwatch stopwatch = new Stopwatch();
+
+        [Benchmark]
+        public long Stopwatch() {
+            return TimeUsingStopwatch(() => 1);
+        }
+
+        [Benchmark]
+        public int BasicTimer() {
+            return basicTimer.Record(() => 1);
+        }
+
+        [Benchmark]
+        public int BasicTimerManual() {
+            return TimeUsingManualOkanshiTimer(() => 1);
+        }
+
+        private T TimeUsingStopwatch<T>(Func<T> f) {
+            stopwatch.Start();
+            var result = f();
+            stopwatch.Stop();
+            return result;
+        }
+
+        private T TimeUsingManualOkanshiTimer<T>(Func<T> f) {
+            var timer = basicTimer.Start();
+            var result = f();
+            timer.Stop();
+            return result;
+        }
+    }
+}
