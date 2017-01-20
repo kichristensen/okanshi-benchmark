a01dc5d7915e6b31f77cb7f968f47f242871f12f
Kim Christensen <KMCH@simcorp.com>
2017-01-20 12:00:07 +0100

    Run all benchmarks

--- OkanshiBenchmark/Program.cs
+++ OkanshiBenchmark/Program.cs
@@ -4,8 +4,7 @@ namespace OkanshiBenchmark {
     public class Program {
         public static void Main() {
             var switcher = new BenchmarkSwitcher(typeof(Program).Assembly);
-            switcher.Run(new [] { "StopwatchVsBasicTimer" });
-            //switcher.RunAll();
+            switcher.RunAll();
         }
     }
 }
